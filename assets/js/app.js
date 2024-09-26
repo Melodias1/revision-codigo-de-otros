8f34d3b78b7a76d36a421af3a95ac5a5170b18b3
Francisco <takincillo@hotmail.com>
2024-09-26 12:10:12 -0600

    se suben cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
-
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+//se cambian las referencias del query selector para que hagan rferencia a las clases existentes
+//las variables se cambian por un nombre las significativo
+const name = document.querySelector('.name');
+const blogP = document.querySelector('.blog');
+const locationP = document.querySelector('.location');
+// se vuelve una async function para poder hacer uso de la funcion await
+ async function displayUser(username) {
+  // se cambian comillas simples por backticks
+  
+  nameP.textContent =`cargando...` ;
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  nameP.textContent = `${data.name}`;
+  blogP.textContent = `${data.blog}`;
+  locationP.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  //no existe variable n, se cambia por name
+  nameP.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
